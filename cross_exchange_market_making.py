3910b7ff46ac21c51ef7c288a644a45c3245d0ed
FourchbarR <cortokaltenbachkb@gmail.com>
2024-10-07 07:05:13 +0200

    Update cross_exchange_market_making.py

--- cross_exchange_market_making.py
+++ cross_exchange_market_making.py
@@ -486,16 +486,6 @@ class CrossExchangeMarketMakingStrategy(StrategyPyBase):
         for order_id in orders_to_cancel:
             self.logger().info(f"Attempting to cancel and replace expired taker order {order_id} with a market order.")
             await self.replace_taker_limit_with_market_order(order_id)
-            # Afficher les valeurs présentes dans les dictionnaires après traitement
-            """
-            self.logger().info(f"After check_taker_order_expiry:")
-            self.logger().info(f"_taker_to_maker_order_ids: {self._taker_to_maker_order_ids}")
-            self.logger().info(f"_maker_to_taker_order_ids: {self._maker_to_taker_order_ids}")
-            self.logger().info(f"_taker_order_timestamps: {self._taker_order_timestamps}")
-            self.logger().info(f"_taker_filled_quantities: {self._taker_filled_quantities}")
-            self.logger().info(f"_ongoing_hedging: {self._ongoing_hedging}")
-            self.logger().info(f"_maker_to_hedging_trades: {self._maker_to_hedging_trades}")
-            """
 
     async def replace_taker_limit_with_market_order(self, order_id: str):
         self.logger().info(f"Starting the process to replace taker limit order {order_id} with a market order.")
@@ -974,15 +964,7 @@ class CrossExchangeMarketMakingStrategy(StrategyPyBase):
                 # Cleanup maker fill events - no longer needed to create taker orders if all fills were hedged
                 if len(self._order_fill_sell_events[market_pair]) == 0:
                     del self._order_fill_sell_events[market_pair]
-                """
-                self.logger().info(f"After check_taker_order_expiry:")
-                self.logger().info(f"_taker_to_maker_order_ids: {self._taker_to_maker_order_ids}")
-                self.logger().info(f"_maker_to_taker_order_ids: {self._maker_to_taker_order_ids}")
-                self.logger().info(f"_taker_order_timestamps: {self._taker_order_timestamps}")
-                self.logger().info(f"_taker_filled_quantities: {self._taker_filled_quantities}")
-                self.logger().info(f"_ongoing_hedging: {self._ongoing_hedging}")
-                self.logger().info(f"_maker_to_hedging_trades: {self._maker_to_hedging_trades}")
-                """
+
     def did_complete_sell_order(self, order_completed_event: SellOrderCompletedEvent):
         """
         Output log message when a ask order (on maker side or taker side) is completely taken.
@@ -1057,15 +1039,7 @@ class CrossExchangeMarketMakingStrategy(StrategyPyBase):
                 # Cleanup maker fill events - no longer needed to create taker orders if all fills were hedged
                 if len(self._order_fill_buy_events[market_pair]) == 0:
                     del self._order_fill_buy_events[market_pair]
-                """
-                self.logger().info(f"After check_taker_order_expiry:")
-                self.logger().info(f"_taker_to_maker_order_ids: {self._taker_to_maker_order_ids}")
-                self.logger().info(f"_maker_to_taker_order_ids: {self._maker_to_taker_order_ids}")
-                self.logger().info(f"_taker_order_timestamps: {self._taker_order_timestamps}")
-                self.logger().info(f"_taker_filled_quantities: {self._taker_filled_quantities}")
-                self.logger().info(f"_ongoing_hedging: {self._ongoing_hedging}")
-                self.logger().info(f"_maker_to_hedging_trades: {self._maker_to_hedging_trades}")
-                """
+
     async def check_if_price_has_drifted(self, market_pair: MakerTakerMarketPair, active_order: LimitOrder):
         """
         Given a currently active limit order on maker side, check if its current price is still valid, based on the
